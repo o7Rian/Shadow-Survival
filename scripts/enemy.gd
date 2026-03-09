071cf44dadad96854dc2433737ce8fabfe0cddbb
o7Rian <104523758+o7Rian@users.noreply.github.com>
2026-03-09 03:02:14 -0400

    Create enemy.gd

---
+++ scripts/enemy.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var speed = 120
+
+var player = null
+
+func _ready():
+    player = get_tree().get_first_node_in_group("player")
+
+func _physics_process(delta):
+
+    if player == null:
+        return
+
+    var direction = (player.global_position - global_position).normalized()
+
+    velocity = direction * speed
+
+    move_and_slide()
